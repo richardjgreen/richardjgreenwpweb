a0326e7ff35476ff95b6dbf46da3a6aef393cf96
Richard Green <richard@baby-green.co.uk>
2014-08-19 19:28:07 +0100

    Update wp-config.php File with Security Improvements

--- wp-config.php
+++ wp-config.php
@@ -4,12 +4,13 @@
 * @package WordPress 
 */ 
 
-define('WP_MEMORY_LIMIT', '128M'); 
+define('WP_SITEURL','http://richardjgreen.net');
+define('WP_HOME','http://richardjgreen.net');
+define('WP_MEMORY_LIMIT', '256M'); 
+define('WP_POST_REVISIONS', 3);
 define('AUTOSAVE_INTERVAL', 120); 
-define('WP_HOME', 'http://richardjgreen.net'); 
-define('WP_SITEURL', 'http://richardjgreen.net'); 
-define('TEMPLATEPATH', "{$_SERVER['HOME_EXPANDED']}\site\wwwroot\wp-content\themes\metro"); 
-define('STYLESHEETPATH', "{$_SERVER['HOME_EXPANDED']}\site\wwwroot\wp-content\themes\metro"); 
+define(‘COOKIE_DOMAIN’, ‘richardjgreen.net’);
+define('FORCE_SSL_ADMIN', true);
  
 define('WP_CACHE', true); 
 define('WPCACHEHOME', "{$_SERVER['HOME_EXPANDED']}\site\wwwroot\wp-content\plugins\wp-super-cache/"); 
@@ -20,24 +21,19 @@ define('DB_PASSWORD', getenv('wp_db_password'));
 define('DB_HOST', getenv('wp_db_host')); 
 define('DB_TYPE', 'pdo_sqlsrv'); 
 define('DB_CHARSET', 'utf8'); 
-define('DB_COLLATE', ''); 
+define('WPLANG', ''); 
  
 $table_prefix  = getenv('wp_db_prefix'); 
  
-define('AUTH_KEY',         'O<_BwDImT)SQxuBVB]HRgfctwo)7`YavUC7<^i-iIH|H[_QlqNq^RCYq*6Df2gwo'); 
-define('SECURE_AUTH_KEY',  '<O2-$1+K-FDc:+<p?^%N]Q&:zkdv&y)-H4yEO0fh_|fQ_IlVNG4vZ,AJH^`*hj]B'); 
-define('LOGGED_IN_KEY',    '=/c8VIex+s7ANiUyQYH^AmRe=~5!6Z[v _[onF#EkNS]2.z!?2nI$ZHbTvz6%X^A'); 
-define('NONCE_KEY',        'X}+y!O-9<|Kz6`Y-4U !>4o[QK{ZXl/fjXc}#(6weSXNWsmdH:r_uF;S>((mssUl'); 
-define('AUTH_SALT',        'isUq7)|2Jax6+u*fvJCH8h.jL?~9?U1.],ee,qk}reS@Ks}0J-r-r+UDa=VpSaa7'); 
-define('SECURE_AUTH_SALT', '[.}2,CY3<a FI(Mu+yAEd,%q@[[$EIO$B-~ 5,7v:@Q)eKT$_QI.uCZmO=v.ppBo'); 
-define('LOGGED_IN_SALT',   'n0Erw1Yt@+eS@Tq_U-hjrx@;kBOt0Hqwwn~:G+2M>b+%[|NT%ZPJ~6T1Jas@;IJ|'); 
-define('NONCE_SALT',       'tAmX7v=|V&&Y&~fye]_h-Qj6jpTdS=<,|k<}|I+#|~cS8b.jPNA6iL]zL(,h%Y:}'); 
- 
-define('SAVEQUERIES', false); 
-define('QUERY_LOG', "{$_SERVER['HOME_EXPANDED']}\site\wwwroot/wp-content\queries.log"); 
-define('WPLANG', ''); 
-define('WP_DEBUG', false); 
- 
+define('AUTH_KEY', getenv('wp_auth_key')); 
+define('SECURE_AUTH_KEY', getenv('wp_secure_auth_key')); 
+define('LOGGED_IN_KEY', getenv('wp_logged_in_key')); 
+define('NONCE_KEY', getenv('wp_nonce_key')); 
+define('AUTH_SALT', getenv('wp_auth_salt')); 
+define('SECURE_AUTH_SALT', getenv('wp_secure_auth_salt')); 
+define('LOGGED_IN_SALT', getenv('wp_logged_in_salt')); 
+define('NONCE_SALT', getenv('wp_nonce_salt')); 
+
 if ( !defined('ABSPATH') ) 
-define('ABSPATH', dirname(__FILE__) . '/'); 
+	define('ABSPATH', dirname(__FILE__) . '/'); 
 require_once(ABSPATH . 'wp-settings.php'); 
